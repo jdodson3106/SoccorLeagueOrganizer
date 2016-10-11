d67b18550a2767996aadccbe4d96eebb2b7e8891
Justin Dodson <blackflamewebdev@gmail.com>
2016-10-11 06:43:18 -0500

    Cleaned and Formatted
    Cleaned up the comments, extra, unused code, and also cleaned up the
    code format to make more readable

--- Organizer.java
+++ Organizer.java
@@ -1,4 +1,4 @@
-package model;
+package com.teamtreehouse.model;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -18,6 +18,8 @@ public class Organizer {
 	private Set<Team> mTeamSet = new LinkedHashSet<Team>();
 	private Map<Team, List<Player>> mTeamRoster = new HashMap<Team, List<Player>>();
 
+	// this is the menu method that will display the menu to the user
+	// this is where the program is controlled. (for now)
 	public void getMenu() {
 		Scanner in = new Scanner(System.in);
 		int choice = 0;
@@ -31,19 +33,18 @@ public class Organizer {
 
 			choice = in.nextInt();
 			switch (choice) {
+			
 			case 1:
 				// This if/else statement controls the amount of teams we can
 				// create.
-				//
 				if (mMasterPlayerList.size() >= 6 && mMasterPlayerList.size() >= mTeamList.size()) {
-					mTeamSet.add(buildNewTeam()); // calls the method to build
-													// new team and adds that
-													// team to the master team
-													// list
-				} else {
+					mTeamSet.add(buildNewTeam()); 
+				} 
+				else {
 					System.out.println("Sorry, not enough players to form another Team.");
 				}
 				break;
+				
 			case 2:
 				int counter = 1;
 				for (Player player : mMasterPlayerList) {
@@ -51,14 +52,15 @@ public class Organizer {
 					counter++;
 				}
 				break;
+				
 			case 3:
 				addPlayerToTeam();
-				// teamSelector();
-
 				break;
+				
 			case 4:
 				removePlayers();
 				break;
+				
 			case 5: // TODO: Team Reports.
 					// To Include: teams grouped by height, and comparison of
 					// experienced vs non experienced players.
@@ -76,26 +78,31 @@ public class Organizer {
 					 * PRODUCE THE CERTAIN REPORT THE ORGANIZER WANTS TO RUN
 					 */
 					switch (selection) {
+					
 					case 1:
 						getByHeight();
-
 						break;
-					// group players by experience
+						
 					case 2:
 						isExperienced();
 						break;
 					case 3:
-						// break to exit switch statement and break the loop
 						break;
+						
+					default:
+						System.out.println("Selection Error. Try another menu option.");	
 					}
+					
 				} while (selection != 3);
 				break;
+				
 			case 6:
 				getRoster();
-				// TODO: Coach report to view team roster.
 				break;
+				
 			case 9:
 				break;
+				
 			default:
 				System.out.println("it no worky...give it another go.");
 			}
@@ -109,10 +116,12 @@ public class Organizer {
 	public Team buildNewTeam() {
 		Scanner in = new Scanner(System.in);
 		System.out.println("What is the team name?");
+		
 		String teamName = in.nextLine();
 		System.out.println("What is the coach's name?");
+		
 		String coachName = in.nextLine();
-
+		
 		return new Team(coachName, teamName);
 	}
 
@@ -123,16 +132,21 @@ public class Organizer {
 	private List<Team> getTeams() {
 		Team[] teams = Teams.load();
 		List<Team> teamList = new ArrayList<Team>(mTeamSet);
+		
 		for (Team team : teams) {
 			teamList.add(team);
 		}
+		
 		Collections.sort(teamList);
+		
 		int counter = 0;
 		for (Team team : teamList) {
 			counter++;
 			System.out.println(counter + ": " + team.getTeam());
 		}
+		
 		mTeamList = teamList;
+		
 		return teamList;
 	}
 
@@ -142,10 +156,13 @@ public class Organizer {
 	 */
 	private static List<Player> createMasterPlayerList() {
 		Player[] newPlayer = Players.load();
+		
 		for (Player player : newPlayer) {
 			mMasterPlayerList.add(player);
 		}
+		
 		Collections.sort(mMasterPlayerList);
+		
 		return mMasterPlayerList;
 	}
 
@@ -163,23 +180,20 @@ public class Organizer {
 	}
 
 	/*
-	 * THIS METHOD WILL BE USED TO CHOOSE THE TEAM TO ALTER, DISPLAY THE TEAM
-	 * LIST AND ADD TEAMS AS WANTED. THIS METHOD NEEDS TO: 1.) PUT THE SELECTED
-	 * TEAM INTO A LOCAL VARIABLE 2.) ADD SELECTED PLAYERS INTO A TEMP LIST (BY
-	 * LOOPING THROUGH AN OPTION MENU) 3.) REMOVE THE SELECTED PLAYERS FROM THE
-	 * MASTER PLAYER LIST 4.) PUT THE TEAM VARIABLE AND TEMP PLAYER LIST INTO
-	 * THE MASTER ROSTER MAP
+	 * This method adds players to a selected team
 	 */
 	public void addPlayerToTeam() {
-		Scanner in = new Scanner(System.in);
+		Scanner in = new Scanner(System.in);	
 		List<Player> teamPlayerList = new ArrayList<Player>();
 		Team team = teamSelector();
 		List<Player> tempList = new ArrayList<Player>(mMasterPlayerList);
+		
 		int selector = 0;
 		do {
 			System.out.println("1: Select player to add to team: ");
 			System.out.println("2: Done editing team:");
 			selector = in.nextInt();
+			
 			switch (selector) {
 			case 1:
 				int counter = 0;
@@ -187,14 +201,22 @@ public class Organizer {
 					counter++;
 					System.out.println(counter + ":" + player.getPlayerStats());
 				}
+				
 				int userChoice = in.nextInt();
+				
 				Player selectedPlayer = tempList.get(userChoice - 1);
+				
 				teamPlayerList.add(selectedPlayer);
+				
 				tempList.remove(userChoice - 1);
+				
 				mMasterPlayerList.remove(userChoice - 1);
+				
 				break;
+				
 			case 2:
 				break;
+				
 			}
 		} while (selector != 2);
 		mTeamRoster.put(team, teamPlayerList);
@@ -203,22 +225,20 @@ public class Organizer {
 	/*
 	 * This method will allow players to be removed from team and placed back in
 	 * the master player list.
-	 * 
-	 * THIS METHOD NEEDS TO: 1.) PUT THE SELECTED TEAM INTO A LOCAL VARIABLE 2.)
-	 * DISPLAY THE PLAYERS OF THAT TEAM 3.) WHEN THE PLAYER IS SELECTED, THE
-	 * PLAYER WILL BE REMOVED FROM THAT ROSTER AND ADDED BACK TO THE MASTER
-	 * PLAYER LIST
-	 */
+	*/
 	private void removePlayers() {
 		Scanner in = new Scanner(System.in);
 		Scanner input = new Scanner(System.in);
 		Team team = teamSelector();
 		List<Player> playerList = mTeamRoster.get(team);
+		
 		int choice = 0;
 		do {
 			System.out.println("1: Select player to remove.");
 			System.out.println("2: Done removing player(s)");
+			
 			choice = in.nextInt();
+			
 			switch (choice) {
 			case 1:
 				int counter = 1;
@@ -227,12 +247,18 @@ public class Organizer {
 					counter++;
 				}
 				System.out.println("Select the player number you want to remove from team (press 'x' to exit): ");
+				
 				int playerChoice = input.nextInt();
+				
 				Player selectedPlayer = playerList.get(playerChoice - 1);
+				
 				playerList.remove(playerChoice - 1);
+				
 				mMasterPlayerList.add(selectedPlayer);
+				
 				Collections.sort(mMasterPlayerList);
 				break;
+				
 			case 2:
 				break;
 			}
@@ -246,13 +272,18 @@ public class Organizer {
 		}
 	}
 	
+	//This method sorts the players on a selected team by height
 	public void getByHeight() {
 		Team team = teamSelector();
 		List<Player> playerList = mTeamRoster.get(team);
+		
 		for (int i = 0; i < playerList.size()-1; i++) {
 			Player min = playerList.get(i);
+			
 			for(int j = i+1; j < playerList.size()-1; j++) {
+				
 				if(playerList.get(j).getHeightInInches() < playerList.get(i).getHeightInInches()) {
+					
 					min = playerList.get(j);
 				}
 				Player temp = playerList.get(i);
@@ -261,12 +292,15 @@ public class Organizer {
 			}
 		}
 	}
-		
+	
+	//This method gives a count, for each team, the amount of experienced and inexperienced players.
 	public void isExperienced() {
+		
 		for (Team team : mTeamList) {
 			List<Player> playerList = mTeamRoster.get(team);
 			int experiencedCount = 0;
 			int inexperiencedCount = 0;
+			
 			for (Player player : playerList) {
 				if(player.isPreviousExperience()) {
 					experiencedCount++;
@@ -275,18 +309,11 @@ public class Organizer {
 					inexperiencedCount++;
 				}
 			}
+			
 			System.out.println("\n" + team + ":\nExperienced players: " + experiencedCount +
 								"\nInexperienced players: " + inexperiencedCount);
 		}
 		
 	}
 	
-	/*
-	 * TODO: create comparator class to sort the teams alphabetically. Once they
-	 * are sorted we can then utilize their / new index location to assign the
-	 * chosen team to a map. I can then use the same logic to alphabatize the
-	 * players, create an empty list for each team roster, add the players to
-	 * the designated roster List that is joined to the team, and add that list
-	 * to the Roster map as the player list.
-	 */
 }
